319de6dd5e7b4c1013bee54701e6f4032f1b7338
Anuradha Gunasekara <saggunasekara@gmail.com>
2021-09-19 22:06:04 +0530

    BinaryGap

---
+++ Iterations/BinaryGap.js
@@ -0,0 +1,10 @@
+function solution(N) {
+    const binary =  N.toString(2);  // convert to binary
+    const x = (binary.match(/(?!1)(0+)(?=1)/g) || []); // match zeros between ones
+    if(!x.length) { // if x is empty no binary gaps
+        return 0
+    }
+    const lenArr = x.map(s => s.length); // map zeros to zero length
+    return Math.max(...lenArr) // returns the max len zeros
+}
+}
